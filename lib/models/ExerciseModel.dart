d18d16acf7cd9efe8346f1ef47fffd9bdc219c04
Kyle Bello <112905761+HeyKile@users.noreply.github.com>
2024-11-18 23:42:44 -0500

    almost

--- lib/models/ExerciseModel.dart
+++ lib/models/ExerciseModel.dart
@@ -2,11 +2,13 @@ class Exercise {
   final int id;
   final String name;
   final List<ExerciseStat> trackedStats;
+  final bool isCustom;
 
   Exercise({
     required this.id,
     required this.name,
-    required this.trackedStats
+    required this.trackedStats,
+    required this.isCustom
   });
 
   factory Exercise.fromJson(Map<String, dynamic> json) {
@@ -15,7 +17,53 @@ class Exercise {
     return Exercise(
       id: json['id'],
       name: json['name'],
-      trackedStats: statsList
+      trackedStats: statsList,
+      isCustom: json['isCustom'] ?? false,
+    );
+  }
+
+  factory Exercise.fromInput(String name, bool hasDistance, bool hasRep, bool hasWeight, bool hasTime) {
+    List<ExerciseStat> statsToTrack = [];
+    if (hasDistance) {
+      statsToTrack.add(
+        const ExerciseStat(
+          type: TrackableStat.distance,
+          display: "Distance",
+          unit: "mi"
+        )
+      );
+    }
+    if (hasRep) {
+      statsToTrack.add(
+        const ExerciseStat(
+          type: TrackableStat.reps,
+          display: "Reps",
+        )
+      );
+    }
+    if (hasWeight) {
+      statsToTrack.add(
+        const ExerciseStat(
+          type: TrackableStat.weight,
+          display: "Weight",
+          unit: "lbs"
+        )
+      );
+    }
+    if (hasTime) {
+      statsToTrack.add(
+        const ExerciseStat(
+          type: TrackableStat.time,
+          display: "Time",
+          unit: "mins"
+        )
+      );
+    }
+    return Exercise(
+      id: -1,
+      name: name,
+      trackedStats: statsToTrack,
+      isCustom: true
     );
   }
 
--- lib/views/widgets/AddExerciseModal.dart
+++ lib/views/widgets/AddExerciseModal.dart
@@ -1,7 +1,7 @@
-import 'package:exercise_tracking_app/viewmodels/ExerciseViewModel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../models/ExerciseModel.dart';
+import '../../viewmodels/ExerciseViewModel.dart';
 
 class AddExerciseModal extends StatefulWidget {
   const AddExerciseModal({super.key,});
@@ -12,71 +12,299 @@ class AddExerciseModal extends StatefulWidget {
 
 class _AddExerciseModalState extends State<AddExerciseModal> {
   List<Exercise> selectedExercises = [];
+  List<Exercise> currentExercises = [];
+  TextEditingController searchController = TextEditingController();
+  ExerciseTab _currentTab = ExerciseTab.allExercises;
 
   @override
   void initState() {
     super.initState();
+    searchController.addListener(_onSearchChanged);
+    Provider.of<ExerciseViewModel>(context, listen: false).fetchExercises();
   }
 
   @override
   void dispose() {
+    searchController.removeListener(_onSearchChanged);
+    searchController.dispose();
     super.dispose();
+  }
+
+  void _onSearchChanged() {
+    Provider.of<ExerciseViewModel>(context, listen: false).getCurrentExercises(
+      _currentTab,
+      searchController.text
+    );
+  }
+
+  void _onCreateExercise(context) async {
+    bool hasDistance = false;
+    bool hasReps = false;
+    bool hasWeight = false;
+    bool hasTime = false;
+
+    TextEditingController nameController = TextEditingController();
+    bool isButtonDisabled = true;
+
+    var res = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return SimpleDialog(
+              title: const Text("Create new exercise"),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: TextField(
+                    controller: nameController,
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
+                    decoration: const InputDecoration(
+                      hintText: 'Exercise Name',
+                      enabledBorder: UnderlineInputBorder(),
+                      focusedBorder: UnderlineInputBorder(),
+                      fillColor: Colors.transparent,
+                      filled: true,
+                      prefixIcon: Icon(Icons.create),
+                    ),
+                    cursorColor: Colors.black,
+                    onChanged: (text) {
+                      setState(() {
+                        isButtonDisabled = text.isEmpty;
+                      });
+                    },
+                  ),
+                ),
+                CheckboxListTile(
+                  title: const Text('Has Distance'),
+                  value: hasDistance,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      hasDistance = value ?? false;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: const Text('Has Reps'),
+                  value: hasReps,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      hasReps = value ?? false;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: const Text('Has Weight'),
+                  value: hasWeight,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      hasWeight = value ?? false;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: const Text('Has Time'),
+                  value: hasTime,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      hasTime = value ?? false;
+                    });
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      foregroundColor: WidgetStateProperty.all(Colors.blue),
+                    ),
+                    onPressed: isButtonDisabled ? null : () {
+                      Navigator.of(context).pop({
+                        'name': nameController.text,
+                        'hasDistance': hasDistance,
+                        'hasReps': hasReps,
+                        'hasWeight': hasWeight,
+                        'hasTime': hasTime,
+                      });
+                    },
+                    child: const Text('Create Exercise'),
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    );
+    if (res != null) {
+      setState(() {
+        Provider.of<ExerciseViewModel>(context, listen: false).addCustomWorkout(
+          res['name'],
+          res['hasDistance'],
+          res['hasReps'],
+          res['hasWeight'],
+          res['hasTime']
+        );
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    double tileWidth = width <= 1000
+      ? width * 0.05
+      : width * 0.1;
+    
+    Color enabledBackgroundColor = Colors.blue[400]!;
+    Color disabledBackgroundColor = Colors.grey[100]!;
+    Color enabledForegroundColor = Colors.black;
+    Color disabledForegroundColor = Colors.black38;
+    
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add Exercise')
+        centerTitle: true,
+        title: TextField(
+          controller: searchController,
+          style: const TextStyle(
+            fontSize: 24,
+          ),
+          decoration: const InputDecoration(
+            hintText: 'Search exercises',
+            enabledBorder: UnderlineInputBorder(),
+            focusedBorder: UnderlineInputBorder(),
+            fillColor: Colors.transparent,
+            filled: true,
+            prefixIcon: Icon(Icons.search),
+          ),
+          cursorColor: Colors.black,
+        ),
+        surfaceTintColor: Colors.grey,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(height * 0.05),
+          child: Column(
+            children: [
+              SegmentedButton<ExerciseTab>(
+                segments: const <ButtonSegment<ExerciseTab>>[
+                  ButtonSegment(
+                    value: ExerciseTab.allExercises,
+                    label: Text('All Exercises')
+                  ),
+                  ButtonSegment(
+                    value: ExerciseTab.customExercises,
+                    label: Text('Custom Exercises')
+                  ),
+                ],
+                selected: {_currentTab},
+                onSelectionChanged: (Set<ExerciseTab> newSection) {
+                  setState(() {
+                    _currentTab = newSection.first;
+                    _onSearchChanged();
+                  });
+                },
+                showSelectedIcon: false,
+                style: SegmentedButton.styleFrom(
+                  selectedBackgroundColor: const Color.fromARGB(200, 33, 150, 243),
+                  selectedForegroundColor: Colors.black,
+                  fixedSize: Size.fromWidth(width),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: width * 0.12
+                  )
+                )
+              ),
+              const SizedBox(height: 10.0),
+            ],
+          )
+        )
       ),
       body: Consumer<ExerciseViewModel>(
         builder: (context, exerciseViewModel, child) {
-          return GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            ),
-            itemCount: exerciseViewModel.exercises.length,
-            itemBuilder: (context, index) {
-              final exercise = exerciseViewModel.exercises[index];
-              final isSelected = selectedExercises.contains(exercise);
-              return GestureDetector(
-                onTap: () {
-                  setState(() {
-                    if (isSelected) {
-                      selectedExercises.remove(exercise);
-                    }
-                    else {
-                      selectedExercises.add(exercise);
-                    }
-                  });
-                },
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    side: BorderSide(
-                      color: isSelected ? Colors.blue : Colors.transparent,
-                      width: 3.0,
+          List<Exercise> curExercises = exerciseViewModel.filteredExercises;
+
+          return Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Visibility(
+                visible: _currentTab == ExerciseTab.customExercises,
+                child: ElevatedButton.icon(
+                  label: const Text("Create New Exercise"),
+                  icon: const Icon(Icons.create),
+                  onPressed: () => _onCreateExercise(context),
+                  style: ButtonStyle(
+                    foregroundColor: WidgetStateProperty.all(
+                      Colors.blue
                     )
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(exercise.name),
-                      Text('ID: ${exercise.id}'),
-                    ],
+                )
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: width > 1000 ? 3 : 2,
                   ),
-                ),
-              );
-            },
+                  padding: EdgeInsets.only(
+                    left: tileWidth,
+                    right: tileWidth,
+                    top: height * 0.01,
+                    bottom: height * 0.2
+                  ),
+                  itemCount: curExercises.length,
+                  itemBuilder: (context, index) {
+                    final exercise = curExercises[index];
+                    final isSelected = selectedExercises.contains(exercise);
+                    
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (isSelected) {
+                            selectedExercises.remove(exercise);
+                          }
+                          else {
+                            selectedExercises.add(exercise);
+                          }
+                        });
+                      },
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          side: BorderSide(
+                            color: isSelected ? Colors.blue : Colors.transparent,
+                            width: 3.0,
+                          )
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(exercise.name),
+                            Text('ID: ${exercise.id}'),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                )
+              )
+            ],
           );
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context, selectedExercises);
-        },
-        child: const Icon(Icons.check),
-      ),
+      floatingActionButton: selectedExercises.isNotEmpty
+        ? FloatingActionButton(
+            backgroundColor: selectedExercises.isEmpty
+              ? disabledBackgroundColor
+              : enabledBackgroundColor,
+            foregroundColor: selectedExercises.isEmpty
+              ? disabledForegroundColor
+              : enabledForegroundColor,
+            onPressed: selectedExercises.isEmpty
+              ? null
+              : () => Navigator.pop(context, selectedExercises),
+            child: const Icon(Icons.check),
+          )
+        : null
     );
   }
 
