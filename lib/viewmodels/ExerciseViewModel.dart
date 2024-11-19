854a53ce0d8462c1405e8c5f4f91dbf234c10831
Kyle Bello <112905761+HeyKile@users.noreply.github.com>
2024-11-18 23:42:44 -0500

    almost

--- lib/viewmodels/ExerciseViewModel.dart
+++ lib/viewmodels/ExerciseViewModel.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import '../models/ExerciseModel.dart';
 import '../services/ExerciseService.dart';
 
+enum ExerciseTab { allExercises, customExercises }
+
 class ExerciseViewModel extends ChangeNotifier {
   List<Exercise> exercises = <Exercise>[];
+  List<Exercise> customExercises = <Exercise>[];
+  List<Exercise> _filteredExercises = <Exercise>[];
 
   final ExerciseService _exerciseService = ExerciseService();
+
+  List<Exercise> get filteredExercises => _filteredExercises;
 
   ExerciseViewModel() {
     fetchExercises();
@@ -13,7 +19,39 @@ class ExerciseViewModel extends ChangeNotifier {
 
   Future<void> fetchExercises() async {
     exercises = await _exerciseService.fetchExercises();
+    getCurrentExercises(ExerciseTab.allExercises, "");
     notifyListeners();
+  }
+
+  void getCurrentExercises(ExerciseTab curTab, String search) {
+    List<Exercise> filteredExercises = exercises;
+    filteredExercises.addAll(customExercises);
+    if (curTab == ExerciseTab.customExercises) {
+      filteredExercises = filteredExercises
+        .where((exercise) => exercise.isCustom)
+        .toList();
+    }
+    if (search != "") {
+      filteredExercises = filteredExercises
+        .where((exercise) => exercise.name.toLowerCase().contains(search.toLowerCase()))
+        .toList();
+    }
+    _filteredExercises = filteredExercises;
+    notifyListeners();
+  }
+
+  Future<bool> addCustomWorkout(String name, bool hasDistance, bool hasRep, bool hasWeight, bool hasTime) async {
+    Exercise exercise = Exercise.fromInput(name, hasDistance, hasRep, hasWeight, hasTime);
+    Map<String, dynamic> exerciseJson = {
+      "id": exercise.id,
+      "name": exercise.name,
+      "isCustom": exercise.isCustom,
+      "trackedStats": exercise.trackedStats
+    };
+    bool res = await _exerciseService.saveExercise(exerciseJson);
+    await fetchExercises();
+    getCurrentExercises(ExerciseTab.customExercises, "");
+    return res;
   }
 
 }
