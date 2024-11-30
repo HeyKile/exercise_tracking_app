4eeb89c0068b1b4214bf96d0262f60e75e53860b
HeyKile <bellokp2002@gmail.com>
2024-11-30 10:51:28 -0500

    removed unused json code for workouts

--- lib/models/WorkoutModel.dart
+++ lib/models/WorkoutModel.dart
@@ -19,57 +19,15 @@ class Workout{
     required this.tags
   }) : id = id ?? const Uuid().v4();
 
-  // workout to json
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'completed': completed.map((exercise) => exercise.toJson()).toList(),
-      'time': time.toString(),
-      'workoutName': workoutName,
-      'date': date.toIso8601String(),
-      'intensity': intensity,
-      'tags': tags.map((tags) => tags.toJson()).toList(),
-    };
-  }
-
   void updateIntensity(int newIntensity) {
     intensity = newIntensity;
   }
-  
-  // json to workout
-  factory Workout.fromJson(Map<String, dynamic> json) {
-    return Workout(
-      id: json['id'] as String,
-      completed: (json['completed'] as List<dynamic>)
-          .map((exercise) => WorkoutExercise.fromJson(exercise))
-          .toList(),
-      time: json['time'] as String,
-      workoutName: json['workoutName'],
-      date: DateTime.parse(json['date']),
-      intensity: json['intensity'],
-      tags: (json['tags'] as List<dynamic>)
-          .map((tag) => Tag.fromJson(tag))
-          .toList(),
-    );
-  }
-
 
 }
 
 class Tag {
   final String name;
 
-  Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-    };
-  }
-
-  factory Tag.fromJson(Map<String, dynamic> json) {
-    return Tag(
-      name: json['name'],
-    );
-  }
   
   Tag({required this.name});
 }
@@ -80,25 +38,12 @@ class WorkoutExercise{
   String notes;
   String? id;
 
-  WorkoutExercise({id, required this.name, required this.sets, required this.notes}) : id = id ?? const Uuid().v4();
-
-  Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'sets': sets.map((sets) => sets.toJson()).toList(),
-      'notes': notes
-    };
-  }
-
-  factory WorkoutExercise.fromJson(Map<String, dynamic> json) {
-    return WorkoutExercise(
-      name: json['name'],
-      sets: (json['sets'] as List<dynamic>)
-      .map((sets) => Set.fromJson(sets))
-      .toList(),
-      notes: json['notes'],
-    );
-  }
+  WorkoutExercise({
+    id,
+    required this.name,
+    required this.sets,
+    required this.notes
+  }) : id = id ?? const Uuid().v4();
 
 }
 
@@ -110,18 +55,4 @@ class Set{
     required this.reps,
     required this.weight,
   });
-
-  Map<String, dynamic> toJson() {
-    return {
-      'reps': reps,
-      'weight': weight,
-    };
-  }
-
-  factory Set.fromJson(Map<String, dynamic> json) {
-    return Set(
-      reps: json['reps'],
-      weight: json['weight'],
-    );
-  }
 }
