d5ae80c99561c4a8bdabd706c67f88a60452bd98
Kyle Bello <112905761+HeyKile@users.noreply.github.com>
2024-12-07 17:29:24 -0500

    code clean-up

--- lib/views/StatsView.dart
+++ lib/views/StatsView.dart
@@ -30,41 +30,59 @@ class _StatsViewState extends State<StatsView> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
+    Color headerBackgroundColor = Colors.transparent;
+
     return SingleChildScrollView(
       child: Column(
         children: [
-          const Text("Your Weekly Workout Split"),
+          Container(
+            margin: const EdgeInsets.only(top: 15.0),
+            child: const Text(
+              "Your Weekly Time Split",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ConstrainedBox(
-                constraints: const BoxConstraints(
+              Container(
+                color: headerBackgroundColor,
+                constraints: BoxConstraints(
                   minWidth: 200,
-                  maxWidth: 500,
+                  maxWidth: width * 0.5,
                   minHeight: 200,
-                  maxHeight: 500,
+                  maxHeight: 250,
                 ),
-                child: PieChart(
-                  PieChartData(
-                    pieTouchData: PieTouchData(
-                      touchCallback: (FlTouchEvent event, pieTouchResponse) {
-                        setState(() {
-                          if (!event.isInterestedForInteractions ||
-                              pieTouchResponse == null ||
-                              pieTouchResponse.touchedSection == null) {
-                            _touchedIndex = -1;
-                            return;
-                          }
-                          _touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
-                        });
-                      },
-                    ),
-                    borderData: FlBorderData(
-                      show: false,
-                    ),
-                    sectionsSpace: 0,
-                    centerSpaceRadius: width * 0.13,
-                    sections: genCurChartSections(width),
-                  ),
+                child: Consumer<StatsViewModel>(
+                  builder: (context, statsViewModel, child) {
+                    return PieChart(
+                      PieChartData(
+                        pieTouchData: PieTouchData(
+                          touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                            setState(() {
+                              if (!event.isInterestedForInteractions ||
+                                  pieTouchResponse == null ||
+                                  pieTouchResponse.touchedSection == null) {
+                                _touchedIndex = -1;
+                                return;
+                              }
+                              _touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
+                            });
+                          },
+                        ),
+                        borderData: FlBorderData(
+                          show: false,
+                        ),
+                        sectionsSpace: 0,
+                        centerSpaceRadius: 60,
+                        sections: statsViewModel.genCurChartSections(_touchedIndex, width),
+                      ),
+                    );
+                  },
                 ),
               ),
             ],
@@ -72,89 +90,5 @@ class _StatsViewState extends State<StatsView> {
         ]
       )
     );
-  }
-
-  List<PieChartSectionData> genCurChartSections(double screenWidth) {
-    return List.generate(3, (i) {
-      final isTouched = i == _touchedIndex;
-      final fontSize = isTouched ? 16.0 : 14.0;
-      final radius = isTouched ? screenWidth * 0.12 : screenWidth * 0.10;
-      final widgetSize = screenWidth * 0.06;
-      const shadows = [Shadow(color: Colors.black, blurRadius: 2)];
-      String title = "";
-      switch (i) {
-        case 0:
-          title = isTouched ? 'Swim (30%)' : 'Swim';
-          break;
-        case 1:
-          title = isTouched ? 'Lift (50%)' : 'Lift';
-          break;
-        case 2:
-          title = isTouched ? 'Run (20%)' : 'Run';
-          break;
-        default:
-          break;
-      }
-      final badgeOffset = isTouched ? screenWidth * 0.0026 : screenWidth * 0.0020;
-
-      switch (i) {
-        case 0:
-          return PieChartSectionData(
-            color: Colors.blue[400],
-            value: 3,
-            title: title,
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: const Color(0xffffffff),
-              shadows: shadows,
-            ),
-            badgeWidget: Icon(
-              Icons.pool,
-              size: widgetSize,
-            ),
-            badgePositionPercentageOffset: badgeOffset,
-          );
-        case 1:
-          return PieChartSectionData(
-            color: Colors.yellow[800],
-            value: 5,
-            title: title,
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: const Color(0xffffffff),
-              shadows: shadows,
-            ),
-            badgeWidget: Icon(
-              Icons.fitness_center,
-              size: widgetSize,
-            ),
-            badgePositionPercentageOffset: badgeOffset,
-          );
-        case 2:
-          return PieChartSectionData(
-            color: Colors.green[600],
-            value: 2,
-            title: title,
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: const Color(0xffffffff),
-              shadows: shadows,
-            ),
-            badgeWidget: Icon(
-              Icons.directions_run,
-              size: widgetSize,
-            ),
-            badgePositionPercentageOffset: badgeOffset,
-          );
-        default:
-          return PieChartSectionData();
-      }
-    });
   }
 }
