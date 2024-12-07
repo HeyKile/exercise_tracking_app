74380992295516b9bb6cb54564ec409bef05698f
Kyle Bello <112905761+HeyKile@users.noreply.github.com>
2024-12-07 11:58:09 -0500

    made pie chart sections more dynamic

--- lib/views/StatsView.dart
+++ lib/views/StatsView.dart
@@ -30,53 +30,79 @@ class _StatsViewState extends State<StatsView> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    return Center(
-      child: AspectRatio(
-        aspectRatio: 1,
-        child: PieChart(
-          PieChartData(
-            pieTouchData: PieTouchData(
-              touchCallback: (FlTouchEvent event, pieTouchResponse) {
-                setState(() {
-                  if (!event.isInterestedForInteractions ||
-                      pieTouchResponse == null ||
-                      pieTouchResponse.touchedSection == null) {
-                    _touchedIndex = -1;
-                    return;
-                  }
-                  _touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
-                });
-              },
-            ),
-            borderData: FlBorderData(
-              show: false,
-            ),
-            sectionsSpace: 0,
-            centerSpaceRadius: width * 0.2,
-            sections: genCurChartSections(),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const Text("Your Weekly Workout Split"),
+          Row(
+            children: [
+              ConstrainedBox(
+                constraints: const BoxConstraints(
+                  minWidth: 200,
+                  maxWidth: 500,
+                  minHeight: 200,
+                  maxHeight: 500,
+                ),
+                child: PieChart(
+                  PieChartData(
+                    pieTouchData: PieTouchData(
+                      touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                        setState(() {
+                          if (!event.isInterestedForInteractions ||
+                              pieTouchResponse == null ||
+                              pieTouchResponse.touchedSection == null) {
+                            _touchedIndex = -1;
+                            return;
+                          }
+                          _touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
+                        });
+                      },
+                    ),
+                    borderData: FlBorderData(
+                      show: false,
+                    ),
+                    sectionsSpace: 0,
+                    centerSpaceRadius: width * 0.13,
+                    sections: genCurChartSections(width),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ),
-      ),
+        ]
+      )
     );
   }
 
-  List<PieChartSectionData> genCurChartSections() {
+  List<PieChartSectionData> genCurChartSections(double screenWidth) {
     return List.generate(3, (i) {
       final isTouched = i == _touchedIndex;
-      final fontSize = isTouched ? 20.0 : 16.0;
-      final radius = isTouched ? 110.0 : 100.0;
-      const widgetSize = 40.0;
+      final fontSize = isTouched ? 16.0 : 14.0;
+      final radius = isTouched ? screenWidth * 0.12 : screenWidth * 0.10;
+      final widgetSize = screenWidth * 0.06;
       const shadows = [Shadow(color: Colors.black, blurRadius: 2)];
+      String title = "";
+      switch (i) {
+        case 0:
+          title = isTouched ? 'Swim (30%)' : 'Swim';
+          break;
+        case 1:
+          title = isTouched ? 'Lift (50%)' : 'Lift';
+          break;
+        case 2:
+          title = isTouched ? 'Run (20%)' : 'Run';
+          break;
+        default:
+          break;
+      }
+      final badgeOffset = isTouched ? screenWidth * 0.0026 : screenWidth * 0.0020;
 
       switch (i) {
         case 0:
-          String curTitle = isTouched
-            ? 'Swim (30%)'
-            : 'Swim';
           return PieChartSectionData(
             color: Colors.blue[400],
             value: 3,
-            title: curTitle,
+            title: title,
             radius: radius,
             titleStyle: TextStyle(
               fontSize: fontSize,
@@ -84,22 +110,17 @@ class _StatsViewState extends State<StatsView> {
               color: const Color(0xffffffff),
               shadows: shadows,
             ),
-            badgeWidget: const Icon(
+            badgeWidget: Icon(
               Icons.pool,
               size: widgetSize,
             ),
-            badgePositionPercentageOffset: isTouched
-              ? 1.13
-              : 0.98,
+            badgePositionPercentageOffset: badgeOffset,
           );
         case 1:
-        String curTitle = isTouched
-            ? 'Lift (50%)'
-            : 'Lift';
           return PieChartSectionData(
             color: Colors.yellow[800],
             value: 5,
-            title: curTitle,
+            title: title,
             radius: radius,
             titleStyle: TextStyle(
               fontSize: fontSize,
@@ -107,22 +128,17 @@ class _StatsViewState extends State<StatsView> {
               color: const Color(0xffffffff),
               shadows: shadows,
             ),
-            badgeWidget: const Icon(
+            badgeWidget: Icon(
               Icons.fitness_center,
               size: widgetSize,
             ),
-            badgePositionPercentageOffset: isTouched
-              ? 1.13
-              : 0.98,
+            badgePositionPercentageOffset: badgeOffset,
           );
         case 2:
-          String curTitle = isTouched
-            ? 'Run (20%)'
-            : 'Run';
           return PieChartSectionData(
             color: Colors.green[600],
             value: 2,
-            title: curTitle,
+            title: title,
             radius: radius,
             titleStyle: TextStyle(
               fontSize: fontSize,
@@ -130,13 +146,11 @@ class _StatsViewState extends State<StatsView> {
               color: const Color(0xffffffff),
               shadows: shadows,
             ),
-            badgeWidget: const Icon(
+            badgeWidget: Icon(
               Icons.directions_run,
               size: widgetSize,
             ),
-            badgePositionPercentageOffset: isTouched
-              ? 1.13
-              : 0.98,
+            badgePositionPercentageOffset: badgeOffset,
           );
         default:
           return PieChartSectionData();
