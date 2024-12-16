654a87ebecc34ba7c358fb43218bbbf2672d0e9e
Julianne <juk99@pitt.edu>
2024-12-15 19:53:44 -0500

    finished stats history

--- lib/viewmodels/StatsViewModel.dart
+++ lib/viewmodels/StatsViewModel.dart
@@ -73,13 +73,148 @@ class StatsViewModel extends ChangeNotifier {
   List<ExerciseStats> genExerciseStats(){
     List<ExerciseStats> trackedExercises = <ExerciseStats>[]; 
 
-    trackedExercises.add(ExerciseStats(exerciseName: "Squats", exerciseId: 1, goalThreshold: 150.0, currPR: 140, category: "Lift"));
-    trackedExercises.add(ExerciseStats(exerciseName: "Bench Press", exerciseId: 2, goalThreshold: 100, currPR: 60, category: "Lift"));
-    trackedExercises.add(ExerciseStats(exerciseName: "Hammer Curls", exerciseId: 3, goalThreshold: 40, currPR: 35, category: "Lift"));
-    trackedExercises.add(ExerciseStats(exerciseName: "5K", exerciseId: 4, goalThreshold: 21, currPR: 22.4, category: "Run"));
-    trackedExercises.add(ExerciseStats(exerciseName: "100 Free", exerciseId: 5, goalThreshold: 58.3, currPR: 59.45, category: "Swim"));
-    trackedExercises.add(ExerciseStats(exerciseName: "100 Fly", exerciseId: 6, goalThreshold: 68, currPR: 68.5, category: "Swim"));
+    trackedExercises.add(ExerciseStats(lowest: 100, exerciseName: "Squats", exerciseId: 1, goalThreshold: 150.0, currPR: 140, category: "Lift",
+    history: genExerciseHistory("Squats")));
+    trackedExercises.add(ExerciseStats(lowest: 40, exerciseName: "Bench Press", exerciseId: 2, goalThreshold: 100, currPR: 60, category: "Lift", history: genExerciseHistory("Bench Press")));
+    trackedExercises.add(ExerciseStats(lowest: 25, exerciseName: "Hammer Curls", exerciseId: 3, goalThreshold: 40, currPR: 35, category: "Lift", history: genExerciseHistory("Hammer Curls")));
+    trackedExercises.add(ExerciseStats(lowest: 25, exerciseName: "5K", exerciseId: 4, goalThreshold: 21, currPR: 22.4, category: "Run", history: genExerciseHistory("5K")));
+    trackedExercises.add(ExerciseStats(lowest: 61, exerciseName: "100 Free", exerciseId: 5, goalThreshold: 58.3, currPR: 59.45, category: "Swim", history: genExerciseHistory("100 Free")));
+    trackedExercises.add(ExerciseStats(lowest: 70, exerciseName: "100 Fly", exerciseId: 6, goalThreshold: 68, currPR: 68.5, category: "Swim", history: genExerciseHistory("100 Fly")));
     return trackedExercises;
+  }
+  List<FlSpot> genExerciseChartData(String exerciseName){
+    switch (exerciseName) {
+        case "Squats":
+          return [
+            FlSpot(0, 100),
+            FlSpot(2.6, 110),
+            FlSpot(4.9, 110),
+            FlSpot(6.8, 115),
+            FlSpot(8, 120),
+            FlSpot(9.5, 135),
+            FlSpot(11, 140),
+          ];
+        case "Bench Press":
+          return [
+            FlSpot(0, 40),
+            FlSpot(2.6, 40),
+            FlSpot(4.9, 50),
+            FlSpot(6.8, 55),
+            FlSpot(8, 55),
+            FlSpot(9.5, 60),
+            FlSpot(11, 55),
+          ];
+        case "Hammer Curls":
+          return [
+            FlSpot(0, 25),
+            FlSpot(2.6, 25),
+            FlSpot(4.9, 30),
+            FlSpot(6.8, 30),
+            FlSpot(8, 30),
+            FlSpot(9.5, 35),
+            FlSpot(11, 35),
+          ];
+        case "5K":
+          return [
+            FlSpot(0, 25),
+            FlSpot(2.6, 25.5),
+            FlSpot(4.9, 24),
+            FlSpot(6.8, 24.2),
+            FlSpot(8, 23.7),
+            FlSpot(9.5, 22.4),
+            FlSpot(11, 23.6),
+          ];
+        case "100 Free":
+          return [
+            FlSpot(0, 60),
+            FlSpot(2.6, 61),
+            FlSpot(4.9, 60.2),
+            FlSpot(6.8, 60.4),
+            FlSpot(8, 60),
+            FlSpot(9.5, 59.6),
+            FlSpot(11, 59.45),
+          ];
+        case "100 Fly":
+          return [
+            FlSpot(0, 70),
+            FlSpot(2.6, 69.7),
+            FlSpot(4.9, 69.4),
+            FlSpot(6.8, 69),
+            FlSpot(8, 69),
+            FlSpot(9.5, 68.5),
+            FlSpot(11, 68.7),
+          ];
+        default:
+          return [];
+      }
+
+  }
+  List<History> genExerciseHistory(String exerciseName){
+    switch (exerciseName) {
+        case "Squats":
+          return [
+            History(date:"9/14", amount: 100),
+            History(date:"9/27", amount: 110),
+            History(date:"9/27", amount: 110),
+            History(date:"10/25", amount: 115),
+            History(date:"11/13", amount: 120),
+            History(date:"11/28", amount: 135),
+            History(date:"12/3", amount: 140),
+          ];
+        case "Bench Press":
+          return [
+            History(date:"9/14", amount: 40),
+            History(date:"9/27", amount: 40),
+            History(date:"9/27", amount: 50),
+            History(date:"10/25", amount: 55),
+            History(date:"11/13", amount: 55),
+            History(date:"11/28", amount: 60),
+            History(date:"12/3", amount: 55),
+          ];
+        case "Hammer Curls":
+          return [
+            History(date:"9/14", amount: 25),
+            History(date:"9/27", amount: 25),
+            History(date:"9/27", amount: 30),
+            History(date:"10/25", amount: 30),
+            History(date:"11/13", amount: 30),
+            History(date:"11/28", amount: 35),
+            History(date:"12/3", amount: 35),
+          ];
+        case "5K":
+          return [
+            History(date:"9/14", amount: 25),
+            History(date:"9/27", amount: 25.5),
+            History(date:"9/27", amount: 24),
+            History(date:"10/25", amount: 24.2),
+            History(date:"11/13", amount: 23.7),
+            History(date:"11/28", amount: 22.4),
+            History(date:"12/3", amount: 23.6),
+          ];
+        case "100 Free":
+          return [
+            History(date:"9/14", amount: 60),
+            History(date:"9/27", amount: 61),
+            History(date:"9/27", amount: 60.2),
+            History(date:"10/25", amount: 60.4),
+            History(date:"11/13", amount: 60),
+            History(date:"11/28", amount: 59.6),
+            History(date:"12/3", amount: 59.45),
+          ];
+        case "100 Fly":
+          return [
+            History(date:"9/14", amount: 70),
+            History(date:"9/27", amount: 69.7),
+            History(date:"9/27", amount: 69.4),
+            History(date:"10/25", amount: 69),
+            History(date:"11/13", amount: 69),
+            History(date:"11/28", amount: 68.5),
+            History(date:"12/3", amount: 68.7),
+          ];
+        default:
+          return [];
+      }
+
   }
 
 }
@@ -91,6 +226,15 @@ class ExerciseStats {
   double goalThreshold;
   double currPR;
   String category;
+  double lowest;
+  List<History> history;
 
-  ExerciseStats({required this.exerciseName, required this.exerciseId, required this.goalThreshold, required this.currPR, required this.category});
+  ExerciseStats({required this.exerciseName, required this.exerciseId, required this.goalThreshold, required this.currPR, required this.category, required this.lowest, required this.history});
+}
+
+class History {
+  String date;
+  double amount;
+
+  History({required this.date, required this.amount});
 }
--- lib/views/widgets/ExerciseStatPopup.dart
+++ lib/views/widgets/ExerciseStatPopup.dart
@@ -1,9 +1,10 @@
 import 'package:exercise_tracking_app/models/ExerciseModel.dart';
-
+import 'package:fl_chart/fl_chart.dart';
 import '../../viewmodels/StatsViewModel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:exercise_tracking_app/views/MainView.dart';
+import '../../viewmodels/StatsViewModel.dart';
 
 
 class ExerciseStatPopup extends StatefulWidget{
@@ -18,17 +19,120 @@ class ExerciseStatPopup extends StatefulWidget{
 class _ExerciseStatPopupState extends State<ExerciseStatPopup>{
   @override 
   Widget build(BuildContext context) { 
-    return Scaffold( 
-      body: CloseDisplay()
-    );
+    return Scaffold(
+      body:SingleChildScrollView( 
+      child: Column(
+        children:[
+          Container( 
+            width: MediaQuery.of(context).size.width * 0.95, 
+            height: 30,
+            margin: const EdgeInsets.only(left:15.0, right: 15.0, top: 20.0, bottom: 20.0),
+            //padding: new EdgeInsets.only(left: 30.0, right: 30.0, top: 3.0, bottom: 3.0),
+            decoration: BoxDecoration(color: Color.fromARGB(255, 0, 149, 255), borderRadius: BorderRadius.circular(20)),
+            child: Text(widget.exercise.exerciseName, textAlign: TextAlign.center ,style:TextStyle(color: Colors.white, fontSize: 20.0))),
+            LineChartSample2(exercise: widget.exercise),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(
+            margin: const EdgeInsets.all(15.0),
+            child: const Text(
+              "Goal:",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(15.0),
+            child: Text(
+              '${widget.exercise.goalThreshold}',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+               
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(15.0),
+            child: const Text(
+              "Current Record:",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(15.0),
+            child: Text(
+              '${widget.exercise.currPR}',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                
+              ),
+            ),
+          ),
+          ],),
+            Container(
+            margin: const EdgeInsets.only(top: 15.0),
+            child: const Text(
+              "History",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Column(children: getExerciseStats(widget.exercise)),
+          CloseDisplay()])
+    ));
 }}
 
+class ExerciseStatListItem extends StatelessWidget{
+  const ExerciseStatListItem({required this.date, required this.units, required this.amount});
+  final String units;
+  final String date;
+  final String amount;
+
+    @override
+  Widget build(BuildContext context) {
+    return
+    Container(
+      margin: const EdgeInsets.only(left:15.0, right: 15.0, top: 10.0, bottom: 10.0),
+      padding: new EdgeInsets.only(left: 15.0, top: 3.0, bottom: 3.0),
+      decoration: BoxDecoration(color: Color.fromARGB(255, 0, 149, 255), borderRadius: BorderRadius.circular(20)),
+      child: Row(
+        children: [
+          Expanded(child:Text(date, style: TextStyle(color:Colors.white))), 
+          Expanded(child:Text("$amount $units", style: TextStyle(color:Colors.white))), 
+          ],)
+    );
+  }
+}
+
+List<Widget> getExerciseStats(ExerciseStats exercise){
+  List<Widget> historyList = [];
+  for(History stat in exercise.history){
+    String units = switch(exercise.category){
+      "Run" => "min",
+      "Swim" => "sec",
+      _ => "lbs"
+    };
+    historyList.add(ExerciseStatListItem(date: stat.date, amount: stat.amount.toString(), units: units));
+  }
+  return historyList;
+}
 
 class CloseDisplay extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox( // green + button
+    return Column(children:[SizedBox( // green + button
       width: MediaQuery.of(context).size.width * 0.95, 
       height: 30, 
       child: ElevatedButton(
@@ -61,7 +165,167 @@ class CloseDisplay extends StatelessWidget{
           ],
         ),
       ),
+    )
+    ]);
+  }
+}
+
+class LineChartSample2 extends StatelessWidget {
+  final ExerciseStats exercise;
+  LineChartSample2({super.key, required this.exercise});
+  
+
+  final List<Color> gradientColors = [
+    Colors.cyan,
+    Colors.blue,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        AspectRatio(
+          aspectRatio: 1.70,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              right: 18,
+              left: 12,
+              top: 24,
+              bottom: 12,
+            ),
+            child: LineChart(
+              mainData(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 16,
+    );
+    Widget text;
+    switch (value.toInt()) {
+      case 2:
+        text = const Text('SEP', style: style);
+        break;
+      case 5:
+        text = const Text('OCT', style: style);
+        break;
+      case 8:
+        text = const Text('NOV', style: style);
+        break;
+      default:
+        text = const Text('', style: style);
+        break;
+    }
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      child: text,
+    );
+  }
+
+  LineChartData mainData() {
+    return LineChartData(
+      gridData: FlGridData(
+        show: true,
+        drawVerticalLine: true,
+        horizontalInterval: 2,
+        verticalInterval: 1,
+        getDrawingHorizontalLine: (value) {
+          return const FlLine(
+            color: Colors.blue,
+            strokeWidth: 1,
+          );
+        },
+        getDrawingVerticalLine: (value) {
+          return const FlLine(
+            color: Colors.blue,
+            strokeWidth: 1,
+          );
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        rightTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        topTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 30,
+            interval: 1,
+            getTitlesWidget: bottomTitleWidgets,
+          ),
+        ),
+        leftTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            interval: switch(exercise.category){
+              "Run" => 5,
+              "Swim" => 3,
+              _=>5
+            },
+            reservedSize: 42,
+          ),
+        ),
+      ),
+      borderData: FlBorderData(
+        show: true,
+        border: Border.all(color: const Color(0xff37434d)),
+      ),
+      minX: 0,
+      maxX: 11,
+      minY: switch(exercise.category){
+        "Run" =>
+        exercise.currPR - 5.0,
+        "Swim" =>
+        exercise.currPR - 5.0,
+        _ =>
+          exercise.lowest - 5.0,
+      },
+      maxY:switch(exercise.category){
+        "Run" =>
+        exercise.lowest + 5.0,
+        "Swim" =>
+        exercise.lowest + 5.0,
+        _ =>
+          exercise.currPR + 5.0,
+      },
+      lineBarsData: [
+        LineChartBarData(
+          spots: genChartData(exercise.exerciseName),
+          isCurved: true,
+          gradient: LinearGradient(
+            colors: gradientColors,
+          ),
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: const FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            gradient: LinearGradient(
+              colors: gradientColors
+                  .map((color) => color.withOpacity(0.3))
+                  .toList(),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
 
+List<FlSpot> genChartData(String exerciseName){
+  StatsViewModel statsVM = StatsViewModel();
+  return statsVM.genExerciseChartData(exerciseName);
+}
